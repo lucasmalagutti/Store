e2d4eec2e4ebafa9c064d6d47ddeedd854f16f59
Lucas Carvalho Malagutti <lucas.malaiorgutti@gmail.com>
2025-01-14 21:23:50 -0300

    Queries and Tests, FakeRepositorys

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2873a96c2edf69c561ba290082d2e9473f781768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e60df2c89f3437276422f54fd0d0a75862e552")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
