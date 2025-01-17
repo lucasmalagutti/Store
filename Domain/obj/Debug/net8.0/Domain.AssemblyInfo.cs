8ee8de8030079e29d94507fe157e8ab5d0dfc730
Lucas Carvalho Malagutti <lucas.malaiorgutti@gmail.com>
2025-01-16 22:16:43 -0300

    Commands

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9e60df2c89f3437276422f54fd0d0a75862e552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d4eec2e4ebafa9c064d6d47ddeedd854f16f59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
